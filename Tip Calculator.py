cbcddaad1ab8dad2ca6a3ce4de71ad1417238630
aify6 <njokuifeanyi911@gmail.com>
2022-09-09 13:13:22 +0100

    day3

--- Tip Calculator.py
+++ Tip Calculator.py
@@ -2,6 +2,6 @@ print('Welcome to the tip calculator.')
 bill = int(input('Enter the total wage bill? $'))
 tip_percent = int(input('What percentage tip would you like to give? 10, 12, or 15? '))
 share = int(input('How many people to split the bill? '))
-tip_amount = (tip_percent/bill150)*100
+tip_amount = (tip_percent/bill)*100
 tpp = round((tip_amount/share),2)
 print('Each person should pay: $',tpp)
