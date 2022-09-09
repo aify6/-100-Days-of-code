f61edf0993291458381ebb6f9726a5e42fbc8b5a
aify6 <njokuifeanyi911@gmail.com>
2022-09-09 13:20:47 +0100

    day 1

--- Band name generator.py
+++ Band name generator.py
@@ -1,5 +1,5 @@
-print('Welcome to Band Name Generator')
-city = input('Enter the city you grew up in ?\n')
-pet = input('Enter the name of your pet: \n')
+print('Welcome to This Band Name Generator')
+city = input('Enter your favorite city: \n')
+pet = input('Enter your pet name: \n')
 name = city+ ' ' +pet
 print('Your band name could be ', name)
